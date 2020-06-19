8815ae6fb8b46725ec48c394a7993f00facb8272
PauloGerson <paulogerson81@gmail.com>
2020-06-19 13:45:22 -0300

    Termino da segunda parte

---
+++ scripts/create-point.js
@@ -0,0 +1,23 @@
+document
+.querySelector("select[name=uf")
+.addEventListener("change", () =>{
+    console.log("mudei")
+})
+
+function populateUFs(){
+    const ufselect = document.querySelector("select[name=uf]")
+    
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( state => {
+        ufselect.innerHTML = ufselect.innerHTML + ` <option value = "1"> Valor </option>`
+    } )
+}
+
+populateUFs()
+
+/*fetch("").then(function(res){
+    return res.json()
+}).then(function(date){
+    console.log(date)
+})*/
